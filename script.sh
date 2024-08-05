756fc7d50cb84831453f6bac7dde2097aef200db
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-08-05 15:10:32 +0100

    add print to .sh

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 python gen_0shot.py 19
+echo 'Finished generation for trec-dl-19!'
 python gen_0shot.py 20
+echo 'Finished generation for trec-dl-20!'
 python gen_0shot.py 21
+echo 'Finished generation for trec-dl-21!'
 python gen_0shot.py 22
+echo 'Finished generation for trec-dl-22!'
