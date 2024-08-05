10a697f2bc685a1bec9b7b6eb06d5c24e93df5e8
Fangzheng Tian <ftian@AD.DCS.GLA.AC.UK>
2024-08-05 17:29:40 +0100

    update sh by adding parsing calls

--- script.sh
+++ script.sh
@@ -7,3 +7,11 @@ python gen_0shot.py 21
 echo 'Finished generation for trec-dl-21!'
 python gen_0shot.py 22
 echo 'Finished generation for trec-dl-22!'
+python process_raw_result.py 19
+echo 'Finished parsing for trec-dl-19!'
+python process_raw_result.py 20
+echo 'Finished parsing for trec-dl-20!'
+python process_raw_result.py 21
+echo 'Finished parsing for trec-dl-21!'
+python process_raw_result.py 22
+echo 'Finished parsing for trec-dl-22!'
